158c6def42e38148e359ae4e28012f69417a2c7d
Daniel Lucas <dlsbdaniel@gmail.com>
2019-07-02 20:21:24 -0300

    Creating a model loader and creating a test user

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res, next) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res, next) => {
+  const user = await User.create({
+    name: 'Daniel Lucas',
+    email: 'dlsbdaniel@gmail.com',
+    password_hash: '1231313131323',
+  }).catch(error => console.log(error));
+  return res.json(user);
+});
 
 export default routes;
