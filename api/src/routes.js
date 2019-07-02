3da3a5b2920420d572c61dc9cbc080cd4ee1be39
Daniel Lucas <dlsbdaniel@gmail.com>
2019-07-02 20:40:57 -0300

    Creating a route to create users

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res, next) => {
-  const user = await User.create({
-    name: 'Daniel Lucas',
-    email: 'dlsbdaniel@gmail.com',
-    password_hash: '1231313131323',
-  }).catch(error => console.log(error));
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
